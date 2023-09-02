362cb50b6e8f94a6e2ab87080b6cb7c876201275
unknown <jtm5432@naver.com>
2023-09-02 17:04:37 +0900

    jest unittest추가

--- src/Components/EventTable.tsx
+++ src/Components/EventTable.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, useRef } from 'react';
-
+import { useMemo, Fragment } from 'react';
+import React from 'react';
 interface TableProps {
     data: (string | number)[][];
     style?: React.CSSProperties;
@@ -45,11 +46,12 @@ const EventTable: React.FC<TableProps> = ({ data, style, columnWidths, width, he
     const addArrayLength = (value: any) => {
 
         if (typeof (value) === 'object') {
-            //console.log('addArrayLength', value, Array.isArray(value))
+            //if(value.city==='etc')console.log('addArrayLength', value,value.city)
             let counter = 0;
             for (let ii in value) {
+                //if(value.city==='etc') console.log('object',value[ii])
                 if (typeof (value[ii]) === 'object') counter = counter + addArrayLength(value[ii]);
-                else counter++;
+                else if (ii !== 'count') counter++;
             }
             return counter;
         }
@@ -101,24 +103,25 @@ const EventTable: React.FC<TableProps> = ({ data, style, columnWidths, width, he
         acc[country][region].push({ city, count });
         return acc;
     }, {});
-    const sortedCountryData = React.useMemo(() => {
+    const sortedCountryData = useMemo(() => {
         return Object.entries(countryData).sort(([countryA, regionsA], [countryB, regionsB]) => {
             const countA = Object.values(regionsA as string).reduce(
-                (acc, cities: any) => acc + cities.reduce((cityAcc, cityData) => cityAcc + Number(cityData.count), 0),
+                (acc, cities: any) => acc + cities.reduce((cityAcc: number, cityData: { count: number }) => cityAcc + Number(cityData.count), 0),
                 0
             );
             const countB = Object.values(regionsB as string).reduce(
-                (acc, cities: any) => acc + cities.reduce((cityAcc, cityData) => cityAcc + Number(cityData.count), 0),
+                (acc, cities: any) => acc + cities.reduce((cityAcc: number, cityData: { count: number }) => cityAcc + Number(cityData.count), 0),
                 0
             );
-            
+
+
             if (sortCriterion === 'sortAscending') {
                 return sortAscending ? countA - countB : countB - countA;
             }
             else {
                 const countA = getCountValue(countryA, '', ''); // 수정
                 const countB = getCountValue(countryB, '', ''); // 수정
-                console.log('sortCriterion',countA,countB)
+                console.log('sortCriterion', countA, countB)
                 return sortOrder === 'asc' ? countA - countB : countB - countA;
             }
         });
@@ -129,35 +132,41 @@ const EventTable: React.FC<TableProps> = ({ data, style, columnWidths, width, he
             <table style={{ width: '100%', tableLayout: 'fixed' }}>
                 <thead>
                     <tr>
-                        <th style={{ width: `${columnWidths[0]}%`, textAlign: 'center' }}></th>
-                        <th style={{ width: `${columnWidths[1]}%`, textAlign: 'center' }}>Country / Region / City
+                        <th style={{ width: `${columnWidths ? columnWidths[0] : 'auto'}%`, textAlign: 'center' }}></th>
+                        <th style={{ width: `${columnWidths ? columnWidths[1] : 'auto'}%`, textAlign: 'center' }}>Country / Region / City
                             Country / Region / City
                             <button onClick={handleSort}>
                                 {sortOrder === 'asc' ? '▲' : '▼'}
                             </button>
                         </th>
-                        <th style={{ width: `${columnWidths[2]}%`, textAlign: 'center' }} onClick={toggleSort}>
+                        <th style={{ width: `$${columnWidths ? columnWidths[2] : 'auto'}%`, textAlign: 'center' }} onClick={toggleSort}>
                             Event Count {sortAscending ? '▲' : '▼'}
                         </th>
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        sortedCountryData.map(([country, regions], countryIndex) => {
-                            const countryCount = Object.values(regions).reduce(
-                                (acc, cities: any) => acc + cities.reduce((cityAcc, cityData) => cityAcc + Number(cityData.count), 0),
-                                0
-                            );
+                        sortedCountryData.map(([country, regions]: [string, unknown], countryIndex: number) => {
+                            regions = regions as Record<string, { city: string, count: number }>;
+                            console.log('regions',regions)
+                            let  countryCount;
+                            let sortedRegions;
+                            if (typeof regions === 'object' && regions !== null) {
+                                 countryCount = Object.values(regions ).reduce(
+                                    (acc, cities: any) => acc + cities.reduce((cityAcc: number, cityData: any) => cityAcc + Number(cityData.count), 0),
+                                    0
+                                );
 
-                            const sortedRegions = Object.entries(regions).sort(([regionA, citiesA], [regionB, citiesB]) => {
-                                const countA = getCountValue(country, regionA, ''); // 수정
-                                const countB = getCountValue(country, regionB, ''); // 수정
-                               // console.log('sortOrder',sortOrder)
-                                return sortOrder === 'asc' ? countA - countB : countB - countA;
-                            });
+                                sortedRegions = Object.entries(regions).sort(([regionA, citiesA], [regionB, citiesB]) => {
+                                    const countA = getCountValue(country, regionA, ''); // 수정
+                                    const countB = getCountValue(country, regionB, ''); // 수정
+                                    // console.log('sortOrder',sortOrder)
+                                    return sortOrder === 'asc' ? countA - countB : countB - countA;
+                                });
+                           }
 
                             return (
-                                <React.Fragment key={countryIndex}>
+                                <Fragment key={countryIndex}>
                                     <tr>
                                         <td onClick={() => toggleExpandCountry(country)}>
                                             {expandedCountries.has(country) ? '-' : '+'}
@@ -166,7 +175,7 @@ const EventTable: React.FC<TableProps> = ({ data, style, columnWidths, width, he
                                         <td style={{ paddingLeft: '20px' }}>{countryCount}</td>
                                     </tr>
                                     {expandedCountries.has(country) &&
-                                        sortedRegions.map(([region, cities], regionIndex) => {
+                                        sortedRegions?.map(([region, cities], regionIndex) => {
                                             const regionKey = `${country}-${region}`;
                                             const regionCount = (cities as any[]).reduce((acc, cityData) => acc + Number(cityData.count), 0);
                                             const sortedCities = (cities as any[]).sort((cityA, cityB) => {
@@ -176,7 +185,7 @@ const EventTable: React.FC<TableProps> = ({ data, style, columnWidths, width, he
                                             });
 
                                             return (
-                                                <React.Fragment key={regionIndex}>
+                                                <Fragment key={regionIndex}>
                                                     <tr>
                                                         <td onClick={() => toggleExpandRegion(regionKey)}>
                                                             {expandedRegions.has(regionKey) ? '-' : '+'}
@@ -192,10 +201,10 @@ const EventTable: React.FC<TableProps> = ({ data, style, columnWidths, width, he
                                                                 <td style={{ paddingLeft: '60px' }}>{cityData.count}</td>
                                                             </tr>
                                                         ))}
-                                                </React.Fragment>
+                                                </Fragment>
                                             );
                                         })}
-                                </React.Fragment>
+                                </Fragment>
                             );
                         })}
                 </tbody>
